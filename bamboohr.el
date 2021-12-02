15cc4ddc8db57886ab81eb55828e3d64f710bb79
Gaelan D'costa <gaelan@tulip.com>
2021-12-01 21:35:58 -0500

    empty GPL'd emacs file

---
+++ bamboohr.el
@@ -0,0 +1,26 @@
+;;; bamboohr.el --- Interact with the BambooHR HR web portal service -* lexical-binding: t; -*-
+
+;; Copyright (C) 2021 Gaelan D'costa
+
+;; Author: Gaelan D'costa <gdcosta@gmail.com>
+;; Created: 2021-01-01
+
+;; Keywords: BambooHR
+;; URL: https://github.com/RobotDisco/bamboohr-emacs
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
