71cb21d9be9b62dd3a06c0b0463bf5525a598dbd
Gaelan D'costa <gaelan@tulip.com>
2021-12-02 08:34:51 -0500

    Add necessary customization settings

--- bamboohr.el
+++ bamboohr.el
@@ -1,26 +1,50 @@
-;;; bamboohr.el --- Interact with the BambooHR HR web portal service -* lexical-binding: t; -*-
-
+;;; bamboohr.el --- Interact with the BambooHR.com -*- lexical-binding: t; -*-
+;;
 ;; Copyright (C) 2021 Gaelan D'costa
-
+;;
 ;; Author: Gaelan D'costa <gdcosta@gmail.com>
 ;; Created: 2021-01-01
-
+;;
 ;; Keywords: BambooHR
 ;; URL: https://github.com/RobotDisco/bamboohr-emacs
-
+;;
 ;; This file is not part of GNU Emacs.
-
+;;
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-
+;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-
+;;
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+;;; Commentary:
 
+;;; Change Log:
+
+;;; Code:
+
+(defgroup bamboohr nil
+  "Interact with BambooHR.com."
+  :prefix "bamboohr-"
+  :group 'applications)
+
+(defcustom bamboohr-api-key ""
+  "API Key for interacting with BambooHR's API."
+  :type 'string
+  :group 'bamboohr)
+
+(defcustom bamboohr-subdomain ""
+  "BambooHR account name.
+
+usually found when you access https://<subdomain>.bamboohr.com."
+  :type 'string
+  :group 'bamboohr)
+
+(provide 'bamboohr)
+;;; bamboohr.el ends here
